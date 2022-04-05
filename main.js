881d4899fa82b6e653a6886874044ecde90b576b
Auseth Mason <826102@stma.k12.mn.us>
2022-04-05 11:40:10 -0500

    color buttons

--- main.js
+++ main.js
@@ -50,4 +50,84 @@ function changeBlue() {
 	document.getElementById("hello").innerHTML = "BLUE";
 }
 
-document.getElementById("myButtonred").addEventListener("click", changeBlue);
+document.getElementById("myButtonblue").addEventListener("click", changeBlue);
+
+
+
+function changeYellow() {
+	document.getElementById("hello").style.backgroundColor = "yellow";
+	document.getElementById("hello").innerHTML = "YELLOW";
+}
+
+document.getElementById("myButtonyellow").addEventListener("click", changeYellow);
+
+
+function changeGreen() {
+	document.getElementById("hello").style.backgroundColor = "green";
+	document.getElementById("hello").innerHTML = "GREEN";
+}
+
+document.getElementById("myButtongreen").addEventListener("click", changeGreen);
+
+
+function changePurple() {
+	document.getElementById("hello").style.backgroundColor = "purple";
+	document.getElementById("hello").innerHTML = "PURPLE";
+}
+
+document.getElementById("myButtonpurple").addEventListener("click", changePurple);
+
+
+function changeBrown() {
+	document.getElementById("hello").style.backgroundColor = "brown";
+	document.getElementById("hello").innerHTML = "BROWN";
+}
+
+document.getElementById("myButtonbrown").addEventListener("click", changeBrown);
+
+
+function changePink() {
+	document.getElementById("hello").style.backgroundColor = "pink";
+	document.getElementById("hello").innerHTML = "PINK";
+}
+
+document.getElementById("myButtonpink").addEventListener("click", changePink);
+
+
+function changeWhite() {
+	document.getElementById("hello").style.backgroundColor = "white";
+	document.getElementById("hello").innerHTML = "WHITE";
+}
+
+document.getElementById("myButtonwhite").addEventListener("click", changeWhite);
+
+
+function changeOrange() {
+	document.getElementById("hello").style.backgroundColor = "orange";
+	document.getElementById("hello").innerHTML = "ORANGE";
+}
+
+document.getElementById("myButtonorange").addEventListener("click", changeOrange);
+
+
+function changeDarkGreen() {
+	document.getElementById("hello").style.backgroundColor = "darkgreen";
+	document.getElementById("hello").innerHTML = "DARKGREEN";
+}
+
+document.getElementById("myButtondarkgreen").addEventListener("click", changeDarkGreen);
+
+
+function changeGold() {
+	document.getElementById("hello").style.backgroundColor = "gold";
+	document.getElementById("hello").innerHTML = "GOLD";
+}
+
+document.getElementById("myButtongold").addEventListener("click", changeGold);
+
+
+
+
+
+
+
