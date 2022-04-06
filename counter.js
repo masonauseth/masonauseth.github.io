f925aa3b468097c637f0ba758cff19cf071008fd
Auseth Mason <826102@stma.k12.mn.us>
2022-04-06 12:46:54 -0500

    counter

--- counter.js
+++ counter.js
@@ -3,12 +3,23 @@ var count = 0;
 function subOne()  {
 	count = count - 1;
 	document.getElementById("total").innerHTML = count;
+	if (count == 68) {
+		document.getElementById("total").innerHTML = "NICE!";
+	}
+	
 }
 
 function addOne()  {
 	count = count + 1;
 	document.getElementById("total").innerHTML = count;
+	if (count == 68) {
+		document.getElementById("total").innerHTML = "NICE!";
+	}
 }
+	
+	
+	
+
 
 document.getElementById("minus").addEventListener("click", subOne);
 document.getElementById("plus").addEventListener("click", addOne);
