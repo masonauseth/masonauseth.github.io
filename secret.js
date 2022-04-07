56937c217965a90a3008dd10fc66dedd8ebdf69a
Auseth Mason <826102@stma.k12.mn.us>
2022-04-07 12:25:53 -0500

    password bg Color

--- secret.js
+++ secret.js
@@ -1,3 +1,18 @@
-if(document.getElementById("password").value == "Masonrules"){
-	
+function password () {
+	if(document.getElementById("password").value == "snickerdoodle"){
+		document.getElementById("password").style.backgroundColor = "Green"
+		document.getElementById("password").value = "Access Granted"
+	}
+	else if(document.getElementById("password").value != "snickerdoodle"){
+		document.getElementById("password").style.backgroundColor = "Red"
+		document.getElementById("password").value = "Access Denied"
+	}
 }
+
+
+
+
+
+
+
+document.getElementById("Enter").addEventListener("click", password);
