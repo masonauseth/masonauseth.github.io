ca74ccbf6d0a239a1489e1f71684c6e07ec4ea05
Auseth Mason <826102@stma.k12.mn.us>
2022-04-07 11:45:19 -0500

    Number fun done

--- counter.js
+++ counter.js
@@ -1,9 +1,29 @@
 var count = 0;
 
+
+
+function Divide2()  {
+	count = count / 2;
+	document.getElementById("total").innerHTML = count;
+	if (count == 69) {
+		document.getElementById("total").innerHTML = "NICE!";
+	}
+}
+
+
+function subFive()  {
+	count = count - 5;
+	document.getElementById("total").innerHTML = count;
+	if (count == 69) {
+		document.getElementById("total").innerHTML = "NICE!";
+	}
+}
+
+
 function subOne()  {
 	count = count - 1;
 	document.getElementById("total").innerHTML = count;
-	if (count == 68) {
+	if (count == 69) {
 		document.getElementById("total").innerHTML = "NICE!";
 	}
 	
@@ -12,14 +32,49 @@ function subOne()  {
 function addOne()  {
 	count = count + 1;
 	document.getElementById("total").innerHTML = count;
-	if (count == 68) {
+	if (count == 69) {
 		document.getElementById("total").innerHTML = "NICE!";
 	}
 }
 	
+
+
+function addFive()  {
+	count = count + 5;
+	document.getElementById("total").innerHTML = count;
+	if (count == 69) {
+		document.getElementById("total").innerHTML = "NICE!";
+	}
+}
+
+function multiply2()  {
+	count = count * 2;
+	document.getElementById("total").innerHTML = count;
+	if (count == 69) {
+		document.getElementById("total").innerHTML = "NICE!";
+	}
+}
+
+function Reset()  {
+	count = count = 0;
+	document.getElementById("total").innerHTML = count;
+	if (count == 69) {
+		document.getElementById("total").innerHTML = "NICE!";
+	}
+}
 	
 	
 
 
 document.getElementById("minus").addEventListener("click", subOne);
 document.getElementById("plus").addEventListener("click", addOne);
+document.getElementById("plus5").addEventListener("click", addFive);
+document.getElementById("minus5").addEventListener("click", subFive);
+document.getElementById("divideBy2").addEventListener("click", Divide2);
+document.getElementById("multiplyBy2").addEventListener("click", multiply2);
+document.getElementById("reset").addEventListener("click", Reset);
+
+
+
+
+
