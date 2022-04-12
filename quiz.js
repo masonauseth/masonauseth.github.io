1b04468e4f21d601e5f28468423502f47abf8c19
Auseth Mason <826102@stma.k12.mn.us>
2022-04-12 12:28:13 -0500

    quiz q

--- quiz.js
+++ quiz.js
@@ -1,16 +1,64 @@
 var q1 = document.getElementById("q1");
-var q1result = document.getElementById("q1result");
+var q2 = document.getElementById("q2");
+var q3 = document.getElementById("q3");
+var q4 = document.getElementById("q4");
+var q5 = document.getElementById("q5");
+var q6 = document.getElementById("q1");
+var q7 = document.getElementById("q2");
+var q8 = document.getElementById("q3");
+var q9 = document.getElementById("q4");
+var q10 = document.getElementById("q5");
+var submit = document.getElementById("submit");
+var score = document.getElementById("score");
+var result = 0;
 
-
-function quiz() {
-	if(q1.value == "Onyx")
-	{
-		(q1result).innerHTML = "correct";
-	}
+function calculate() {
+if(q1.value == "Onyx"){
+	result = result + 1;
+}
+if(q2.value == "Osowski"){
+	result = result + 1;
+}
+if(q3.value == "An Aspen Tree forest"){
+	result = result + 1;
+}
+if(q4.value == "Mason"){
+	result = result + 1;
+}
+if(q5.value == "Beginnings"){
+	result = result + 1;
+}
+if(q6.value == "CADD then Marketing"){
+	result = result + 1;
+}
+if(q7.value == "Osowski"){
+	result = result + 1;
+}
+if(q8.value == "An Aspen Tree forest"){
+	result = result + 1;
+}
+if(q9.value == "Mason"){
+	result = result + 1;
+}
+if(q10.value == "Beginnings"){
+	result = result + 1;
+}
 	
-	else {
-		(q1result).innerHTML = "wrong";
-	}
+	
+	
+	
+	
+	
 }
 
-document.getElementById("s1").addEventListener("click", quiz);
+
+
+
+
+
+
+
+
+
+
+
