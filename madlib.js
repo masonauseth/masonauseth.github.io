448c15213168b2d6ae5f97e7d269ed2bc49ea1f4
Auseth Mason <826102@stma.k12.mn.us>
2022-04-18 12:47:13 -0500

    madlibs stuff

--- madlib.js
+++ madlib.js
@@ -1,1 +1,28 @@
-// JavaScript Document
+var name
+var noun1
+var adjective1 
+var adverb
+var color 
+var noun2
+var noun3
+var adjective2 
+var vehicle 
+var noun4
+function madlib() {
+name = document.getElementById("name").value;
+noun2 = document.getElementById("noun1").value;
+adjective1 = document.getElementById("adjective1").value;
+adverb = document.getElementById("adverb").value;
+color = document.getElementById("color").value;
+noun3 = document.getElementById("noun2").value;
+noun4 = document.getElementById("noun3").value;
+adjective2 = document.getElementById("adjective2").value;
+vehicle = document.getElementById("vehicle").value;
+noun5 = document.getElementById("noun4").value;
+	
+document.getElementById("madlibstory").innerHTML = "Once ther was a guy named " + name + ". He lived in a " + naoun1 + " and he loved it."
+}
+
+
+
+
