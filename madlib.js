f4420f7082c6ef2b6903ba7964b53300e56fc728
Auseth Mason <826102@stma.k12.mn.us>
2022-04-20 12:29:09 -0500

    madlib fixed and back to home added

--- madlib.js
+++ madlib.js
@@ -1,26 +1,26 @@
 var name1;
 var noun1;
-var adjective1;
-var adverb;
+var adverb1;
+var adverb2;
 var color;
 var noun2;
 var noun3;
-var adjective2;
+var adverb3;
 var vehicle;
 var noun4;
 function madlib() {
 name1 = document.getElementById("name1").value;
 noun1 = document.getElementById("noun1").value;
-adjective1 = document.getElementById("adjective1").value;
-adverb = document.getElementById("adverb").value;
+adverb1 = document.getElementById("adverb1").value;
+adverb2 = document.getElementById("adverb2").value;
 color = document.getElementById("color").value;
 noun2 = document.getElementById("noun2").value;
 noun3 = document.getElementById("noun3").value;
-adjective2 = document.getElementById("adjective2").value;
+adverb3 = document.getElementById("adverb3").value;
 vehicle = document.getElementById("vehicle").value;
 noun4 = document.getElementById("noun4").value;
 	
-document.getElementById("madlibstory").innerHTML = "Once ther was a guy named " + name1 + ". He lived in a " + noun1 + " and he loved it. Every day he would " + adjective1 + " run the store and buy lots of apples " + adverb + ". He then would would go back to his " + color + " car and drive away. He would drive to a " + noun2 + " and get out his " + noun3 + ". He then " + adjective2 + " hops in a " + vehicle + "and leaves. Some people may say this is weird but it makes perfect sence to him and his" + noun4 + ".";
+document.getElementById("madlibstory").innerHTML = "Once there was a guy named " + name1 + ". He lived in a " + noun1 + " and he loved it. Every day he would " + adverb1 + " run the store and buy lots of apples " + adverb2 + ". He then would would go back to his " + color + " car and drive away. He would drive to a " + noun2 + " and get out his " + noun3 + ". He then " + adverb3 + " hops in a " + vehicle + " and leaves. Some people may say this is weird but it makes perfect sence to him and his " + noun4 + ".";
 }
 
 document.getElementById("madbutt").addEventListener("click", madlib)
