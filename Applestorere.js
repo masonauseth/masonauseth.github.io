08f6e15a7e263a4d8915ffc99c31da4e5c464cd2
Auseth Mason <826102@stma.k12.mn.us>
2022-04-28 12:29:04 -0500

    links baby

---
+++ Applestorere.js
@@ -0,0 +1,55 @@
+
+
+function i13pro () {
+	window.open ("https://www.apple.com/iphone-13-pro/")
+}
+
+function i13 () {
+	window.open ("https://www.apple.com/iphone-13/")
+}
+
+function SE () {
+	window.open ("")
+}
+
+function i12 () {
+	window.open ("")
+}
+
+function i11 () {
+	window.open ("")
+}
+
+function compare () {
+	window.open ("")
+}
+
+function airpods () {
+	window.open ("")
+}
+
+function airtags () {
+	window.open ("")
+}
+
+function acces () {
+	window.open ("")
+}
+
+function card () {
+	window.open ("")
+}
+
+function ios15 () {
+	window.open ("")
+}
+
+function shop () {
+	window.open ("")
+}
+
+
+
+
+
+
