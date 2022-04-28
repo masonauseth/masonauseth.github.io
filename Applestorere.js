6e0e618feb472c3854da688b58155180e1af0bbb
Auseth Mason <826102@stma.k12.mn.us>
2022-04-28 14:12:19 -0500

    fixes

--- Applestorere.js
+++ Applestorere.js
@@ -1,5 +1,54 @@
 
 
+
+
+
+
+
+
+
+
+
+function store () {
+	window.open ("https://www.apple.com/store")
+}
+
+function mac () {
+	window.open ("https://www.apple.com/mac/")
+}
+
+function ipad () {
+	window.open ("https://www.apple.com/ipad/")
+}
+
+function iphone () {
+	window.open ("https://www.apple.com/iphone/")
+}
+
+function watch () {
+	window.open ("")
+}
+
+function AirPods () {
+	window.open ("")
+}
+
+function TV () {
+	window.open ("")
+}
+
+function only () {
+	window.open ("")
+}
+
+function Accessories () {
+	window.open ("")
+}
+
+function Support () {
+	window.open ("")
+}
+
 function i13pro () {
 	window.open ("https://www.apple.com/iphone-13-pro/")
 }
@@ -9,44 +58,68 @@ function i13 () {
 }
 
 function SE () {
-	window.open ("")
+	window.open ("https://www.apple.com/iphone-se/")
 }
 
 function i12 () {
-	window.open ("")
+	window.open ("https://www.apple.com/iphone-12/key-features/")
 }
 
 function i11 () {
-	window.open ("")
+	window.open ("https://www.apple.com/shop/buy-iphone/iphone-11")
 }
 
 function compare () {
-	window.open ("")
+	window.open ("https://www.apple.com/iphone/compare/")
 }
 
 function airpods () {
-	window.open ("")
+	window.open ("https://www.apple.com/airpods/")
 }
 
 function airtags () {
-	window.open ("")
+	window.open ("https://www.apple.com/airtag/")
 }
 
 function acces () {
-	window.open ("")
+	window.open ("https://www.apple.com/shop/iphone/accessories")
 }
 
 function card () {
-	window.open ("")
+	window.open ("https://www.apple.com/apple-card/")
 }
 
 function ios15 () {
-	window.open ("")
+	window.open ("https://www.apple.com/ios/ios-15/")
 }
 
 function shop () {
-	window.open ("")
+	window.open ("https://www.apple.com/shop/buy-iphone")
 }
+
+document.getElementById("i13pro").addEventListener("click", i13pro)
+document.getElementById("i13").addEventListener("click", i13)
+document.getElementById("SE").addEventListener("click", SE)
+document.getElementById("i12").addEventListener("click", i12)
+document.getElementById("i11").addEventListener("click", i11)
+document.getElementById("compare").addEventListener("click", compare)
+document.getElementById("airpods").addEventListener("click", airpods)
+document.getElementById("airtags").addEventListener("click", airtags)
+document.getElementById("acces").addEventListener("click", acces)
+document.getElementById("card").addEventListener("click", card)
+document.getElementById("ios15").addEventListener("click", ios15)
+document.getElementById("shop").addEventListener("click", shop)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
