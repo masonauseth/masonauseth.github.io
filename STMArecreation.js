4c04df4920c40a16d54721b2eda012a221867fd0
Auseth Mason <826102@stma.k12.mn.us>
2022-04-29 12:28:29 -0500

    links on stma

--- STMArecreation.js
+++ STMArecreation.js
@@ -3,3 +3,46 @@ function acti () {
 }
 
 document.getElementById("acti").addEventListener("click", acti)
+
+function event () {
+	window.open ("https://www.stma.k12.mn.us/Page/2#calendar1/20220429/month/")
+}
+
+document.getElementById("event").addEventListener("click", event)
+
+function year () {
+	window.open ("https://www.stma.k12.mn.us/domain/4555")
+}
+
+document.getElementById("year").addEventListener("click", year)
+
+function parent () {
+	window.open ("https://pvue7.region1.k12.mn.us/rt0885/PXP2_Login.aspx")
+}
+
+document.getElementById("parent").addEventListener("click", parent)
+
+function menu () {
+	window.open ("https://stma.nutrislice.com/menus-eula")
+}
+
+document.getElementById("menu").addEventListener("click", menu)
+
+function employment () {
+	window.open ("https://www.stma.k12.mn.us/domain/2433")
+}
+
+document.getElementById("employment").addEventListener("click", employment)
+
+function skyward () {
+	window.open ("https://pac.stma.k12.mn.us/scripts/wsisa.dll/WService=wsFin/seplog01.w")
+}
+
+document.getElementById("skyward").addEventListener("click", skyward)
+
+function staff () {
+	window.open ("https://mail.google.com/mail/u/0/#inbox")
+}
+
+document.getElementById("staff").addEventListener("click", staff)
+
