339238a4d8f3c53bc3f6780da4cef8817d61d13c
Auseth Mason <826102@stma.k12.mn.us>
2022-05-31 12:43:43 -0500

    dung and flukemarm

--- combat.js
+++ combat.js
@@ -1,5 +1,7 @@
 
 var count = 0;
+var count2 = 0;
+var count3 = 0;
 
 function addOne()  {
 	count = count + 1;
@@ -10,3 +12,25 @@ function addOne()  {
 }
 	
 document.getElementById("punchy").addEventListener("click", addOne);
+
+
+function addOne2()  {
+	count2 = count2 + 1;
+	document.getElementById("damagedealt2").innerHTML = "Damage Dealt = " + count2;
+	if (count2 >= 200) {
+		document.getElementById("damagedealt2").innerHTML = "Dead";	
+		}
+}
+	
+document.getElementById("punchy2").addEventListener("click", addOne2);
+
+
+function addOne3()  {
+	count3 = count3 + 1;
+	document.getElementById("damagedealt3").innerHTML = "Damage Dealt = " + count3;
+	if (count3 >= 300) {
+		document.getElementById("damagedealt3").innerHTML = "Dead";	
+		}
+}
+	
+document.getElementById("punchy3").addEventListener("click", addOne3);
